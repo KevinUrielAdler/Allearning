d41b1a1c7050d0d0217e28c9676c82a2940eeea6
Kevin Uriel Manzano Rios <74994335+ElKPROOB@users.noreply.github.com>
2020-12-25 16:04:26 -0600

    Actualizacion de etiquetas

--- Capa de Presentacion/Form1.Designer.cs
+++ Capa de Presentacion/Form1.Designer.cs
@@ -32,7 +32,7 @@ namespace Capa_de_Presentacion
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblregistrate = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.Lbl_IniciarSesion = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.PictureBox();
@@ -71,17 +71,17 @@ namespace Capa_de_Presentacion
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label2
+            // lblregistrate
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(266, 120);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(383, 33);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Registrate si aun no lo estas";
+            this.lblregistrate.AutoSize = true;
+            this.lblregistrate.BackColor = System.Drawing.Color.Transparent;
+            this.lblregistrate.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblregistrate.ForeColor = System.Drawing.Color.White;
+            this.lblregistrate.Location = new System.Drawing.Point(266, 120);
+            this.lblregistrate.Name = "lblregistrate";
+            this.lblregistrate.Size = new System.Drawing.Size(383, 33);
+            this.lblregistrate.TabIndex = 12;
+            this.lblregistrate.Text = "Registrate si aun no lo estas";
             // 
             // label1
             // 
@@ -139,7 +139,7 @@ namespace Capa_de_Presentacion
             this.ClientSize = new System.Drawing.Size(906, 384);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblregistrate);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Lbl_IniciarSesion);
             this.Controls.Add(this.button2);
@@ -159,7 +159,7 @@ namespace Capa_de_Presentacion
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblregistrate;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label Lbl_IniciarSesion;
         private System.Windows.Forms.PictureBox button3;
